7049ee5bf0370d85d49bc7be31c29a55bafc83ee
Zachary Canann <zacharycanann@gmail.com>
2017-12-05 22:35:33 -0800

    Coding style updates
    Former-commit-id: ebec4c79422e51d4a03b84aea305b579b422dcff [formerly ebec4c79422e51d4a03b84aea305b579b422dcff [formerly 7dcdd4be90f3a3932435fb0f74011c5a569059f0]]
    Former-commit-id: 21ba776d80a889b565af55076044159ff2037693
    Former-commit-id: 0ff53dad34921cd4d92295e1aad0c103d325d84e

--- Source/AppDelegate.h
+++ Source/AppDelegate.h
@@ -4,36 +4,36 @@
 #include "cocos2d.h"
 
 /**
-@brief    The cocos2d Application.
+@brief The cocos2d Application.
 
 Private inheritance here hides part of interface from Director.
 */
 class  AppDelegate : private cocos2d::Application
 {
 public:
-    AppDelegate();
-    virtual ~AppDelegate();
+	AppDelegate();
+	virtual ~AppDelegate();
 
-    virtual void initGLContextAttrs();
+	virtual void initGLContextAttrs();
 
-    /**
-    @brief    Implement Director and Scene init code here.
-    @return true    Initialize success, app continue.
-    @return false   Initialize failed, app terminate.
-    */
-    virtual bool applicationDidFinishLaunching();
+	/**
+	@brief Implement Director and Scene init code here.
+	@return true Initialize success, app continue.
+	@return false Initialize failed, app terminate.
+	*/
+	virtual bool applicationDidFinishLaunching();
 
-    /**
-    @brief  Called when the application moves to the background
-    @param  the pointer of the application
-    */
-    virtual void applicationDidEnterBackground();
+	/**
+	@brief Called when the application moves to the background.
+	@param The pointer of the application.
+	*/
+	virtual void applicationDidEnterBackground();
 
-    /**
-    @brief  Called when the application reenters the foreground
-    @param  the pointer of the application
-    */
-    virtual void applicationWillEnterForeground();
+	/**
+	@brief Called when the application reenters the foreground.
+	@param The pointer of the application.
+	*/
+	virtual void applicationWillEnterForeground();
 };
 
 #endif // _APP_DELEGATE_H_
