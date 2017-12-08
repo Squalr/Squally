af77d8b7394746f701ce7e5284873cd1e3f87cfb
Zachary Canann <zacharycanann@gmail.com>
2017-12-07 22:56:48 -0800

    Adding title screen
    Former-commit-id: 9ee5be2e698c8de4976c51f72b7e83d6891e4abf [formerly 9ee5be2e698c8de4976c51f72b7e83d6891e4abf [formerly edeeca327733cf550fa4876e25303822527f64a3]]
    Former-commit-id: 36fdae3ef669875a33a64b8c826ae35186286d16
    Former-commit-id: e75ca6164775110c6a7a07d387484ee5c0212a31

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -1,52 +1,29 @@
 #include "Game.h"
 #include "SimpleAudioEngine.h"
 #include "Resources.h"
+#include "GUI/Screens/TitleScreen.h"
 #include "Level/Level.h"
 
 using namespace cocos2d;
 
-// on "init" you need to initialize your instance
-bool Game::init()
+Game::Game()
 {
-	if (!Scene::init())
-	{
-		return false;
-	}
+	this->titleScreen = new TitleScreen(bind(&Game::GameStart, this));
 
-	Size visibleSize = Director::getInstance()->getVisibleSize();
-	Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
-	MenuItemImage* closeItem = MenuItemImage::create(Resources::CloseNormal, Resources::CloseSelected, CC_CALLBACK_1(Game::menuCloseCallback, this));
-
-	float x = origin.x + visibleSize.width - closeItem->getContentSize().width / 2;
-	float y = origin.y + closeItem->getContentSize().height / 2;
-
-	closeItem->setPosition(Vec2(x, y));
-
-	// create menu, it's an autorelease object
-	Menu* menu = Menu::create(closeItem, NULL);
-
-	menu->setPosition(Vec2::ZERO);
-	this->addChild(menu, 1);
-
-	Label* label = Label::createWithTTF("Squalr Tutorial", Resources::Fonts_Marker_Felt, 24);
-
-	// position the label on the center of the screen
-	label->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height - label->getContentSize().height));
-
-	// add the label as a child to this layer
-	this->addChild(label, 1);
-
-	Level* level = new Level();
-	level->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
-	this->addChild(level, 0);
-
-	return true;
+	// Show level
+	Director::getInstance()->runWithScene(this->titleScreen);
 }
 
-Scene* Game::createScene()
+Game::~Game()
 {
-	return Game::create();
+	delete(this->titleScreen);
+}
+
+void Game::GameStart()
+{
+	Level* level = new Level();
+
+	Director::getInstance()->replaceScene(level);
 }
 
 void Game::menuCloseCallback(Ref* pSender)
