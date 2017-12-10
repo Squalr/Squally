c0ece66085e7c80f91aeb39cb6d4b19ac38d8d02
Zachary Canann <zacharycanann@gmail.com>
2017-12-09 22:08:43 -0800

    Updates to tiles, collision groups, tile map parsing
    Former-commit-id: fe6c019fab614b69450f150303eb5ec2bb2bc094 [formerly fe6c019fab614b69450f150303eb5ec2bb2bc094 [formerly 30aa69a36792aeb5815bec9da062e5f48a3497d0]]
    Former-commit-id: 9837fda55f2d4c62a625abdbed13af238eeefc7c
    Former-commit-id: 55a768055279098da069a7275a729f94678187f4

--- Source/Entities/Player.cpp
+++ Source/Entities/Player.cpp
@@ -1,18 +1,22 @@
 #include "Player.h"
 #include "Resources.h"
+#include "../Collision/CollisionGroups.h"
 #include "cocos2d.h"
 
 using namespace cocos2d;
 
 static int health;
 
+bool canJump;
+
 Player::Player()
 {
 	Entity::Entity();
 
 	this->sprite = Sprite::create(Resources::Sprites_Player_Idle);
-	this->physicsBody = PhysicsBody::createCircle(this->sprite->getContentSize().width / 2);
+	this->physicsBody = PhysicsBody::createCircle(this->sprite->getContentSize().width / 2, PhysicsMaterial(0.0f, 0.0f, 0.0f));
 	this->physicsBody->setRotationEnable(false);
+	this->physicsBody->setContactTestBitmask(CollisionGroups::CollisionGroupsEnum::Entity);
 
 	this->addChild(this->sprite);
 	this->setPhysicsBody(this->physicsBody);
@@ -37,11 +41,42 @@ void Player::update(float dt)
 
 void Player::InitializeListeners()
 {
+	EventListenerPhysicsContact* contactListener = EventListenerPhysicsContact::create();
 	EventListenerKeyboard* listener = EventListenerKeyboard::create();
+
+	contactListener->onContactBegin = CC_CALLBACK_1(Player::OnContactBegin, this);
 	listener->onKeyPressed = CC_CALLBACK_2(Player::OnKeyPressed, this);
 	listener->onKeyReleased = CC_CALLBACK_2(Player::OnKeyReleased, this);
 
-	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+	this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(contactListener, this);
+	this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
+}
+
+bool Player::OnContactBegin(PhysicsContact &contact)
+{
+	Node* nodeA = contact.getShapeA()->getBody()->getNode();
+	Node* nodeB = contact.getShapeB()->getBody()->getNode();
+
+	if (nodeA != this && nodeB != this)
+	{
+		return true;
+	}
+
+	for (int index = 0; index < size(contact.getContactData()->points); index++)
+	{
+		Vec2 contactPoint = contact.getContactData()->points[index];
+
+		float end = this->getPositionX() + this->GetWidth();
+		float x = this->getPositionX();
+		float y = this->getPositionY();
+
+		if (contactPoint.y > y)
+		{
+			canJump = true;
+		}
+	}
+
+	return true;
 }
 
 void Player::OnKeyPressed(EventKeyboard::KeyCode keyCode, Event* event)
@@ -50,7 +85,12 @@ void Player::OnKeyPressed(EventKeyboard::KeyCode keyCode, Event* event)
 	{
 	case EventKeyboard::KeyCode::KEY_UP_ARROW:
 	case EventKeyboard::KeyCode::KEY_W:
-		this->physicsBody->setVelocity(Vec2(this->physicsBody->getVelocity().x, Player::jumpSpeed));
+		// if (canjump)
+		if (this->physicsBody->getVelocity().y > -4.0f && this->physicsBody->getVelocity().y < 4.0f)
+		{
+			canJump = false;
+			this->physicsBody->setVelocity(Vec2(this->physicsBody->getVelocity().x, Player::jumpSpeed));
+		}
 		break;
 	case EventKeyboard::KeyCode::KEY_LEFT_ARROW:
 	case EventKeyboard::KeyCode::KEY_A:
