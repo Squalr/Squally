38988e62164f6608eb2f3df7df76125769fe90d7
Zachary Canann <zacharycanann@gmail.com>
2017-12-10 23:16:44 -0800

    Adding resolution switching and saving

---
+++ Source/Config/ConfigManager.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "cocos2d.h"
+#include "Resources.h"
+
+using namespace cocos2d;
+
+class ConfigManager
+{
+public:
+	enum ResolutionSetting {
+		FullScreen,
+		R1080x768,
+		R1920x1080,
+	};
+
+	static void SetGlView(GLViewImpl* currentGlView);
+	static void SetResolution(ResolutionSetting resolution);
+
+	static ResolutionSetting LoadResolution();
+
+	const static Size* Resolution1024x768;
+	const static Size* Resolution1920x1080;
+
+private:
+	static GLViewImpl* glView;
+};
