fe8946e86af1af969fca6a40533cd5bc243d50e6
Zachary Canann <zacharycanann@gmail.com>
2017-12-12 21:32:14 -0800

    Tutorial nodes clickable
    Former-commit-id: f36293e7932d0df7a8c301d1154c681c8bbd4c0c [formerly f36293e7932d0df7a8c301d1154c681c8bbd4c0c [formerly 73d501b35f49d838a1f096296b37c6508e5f338b]]
    Former-commit-id: de36bbd400f8afb7013ea90ee61006ec489fb13e
    Former-commit-id: aab5f6529ac14cf9a82a712ef72b6b10094dd652

--- Source/GUI/Screens/MapNode.cpp
+++ Source/GUI/Screens/MapNode.cpp
@@ -2,18 +2,25 @@
 
 MapNode::MapNode(Vec2 position, bool tutorialNode)
 {
+	this->setPosition(position);
+
 	if (tutorialNode)
 	{
 		this->sprite = Sprite::create(Resources::GUI_WorldMaps_TutorialNodeUnlocked);
+		this->spriteSelected = Sprite::create(Resources::GUI_WorldMaps_TutorialNodeSelected);
 	}
 	else
 	{
 		this->sprite = Sprite::create(Resources::GUI_WorldMaps_LevelNodeUnlocked);
+		this->spriteSelected = Sprite::create(Resources::GUI_WorldMaps_TutorialNodeSelected);
 	}
 
-	this->sprite->setPosition(position);
+	this->setContentSize(this->sprite->getContentSize());
+
+	this->spriteSelected->setVisible(false);
 
 	this->addChild(this->sprite);
+	this->addChild(this->spriteSelected);
 }
 
 MapNode::~MapNode()
@@ -31,13 +38,31 @@ void MapNode::InitializeListeners()
 {
 	EventListenerMouse* mouseListener = EventListenerMouse::create();
 
+	mouseListener->onMouseMove = CC_CALLBACK_1(MapNode::OnMouseMove, this);
 	mouseListener->onMouseDown = CC_CALLBACK_1(MapNode::OnMouseDown, this);
 
 	this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(mouseListener, this);
 }
 
+void MapNode::OnMouseMove(EventMouse* event)
+{
+	if (Utils::Intersects(this, Vec2(event->getCursorX(), event->getCursorY())))
+	{
+		this->sprite->setVisible(false);
+		this->spriteSelected->setVisible(true);
+	}
+	else
+	{
+		this->sprite->setVisible(true);
+		this->spriteSelected->setVisible(false);
+	}
+}
+
 void MapNode::OnMouseDown(EventMouse* event)
 {
-	Director::getInstance()->replaceScene(new Level());
+	if (Utils::Intersects(this, Vec2(event->getCursorX(), event->getCursorY())))
+	{
+		Director::getInstance()->replaceScene(new Level());
+	}
 }
 
