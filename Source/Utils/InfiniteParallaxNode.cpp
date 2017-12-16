7aea90e2207fea2a1a7c3bf39fb38a9584e48a43
Zachary Canann <zacharycanann@gmail.com>
2017-12-15 16:58:20 -0800

    Parallax updates

--- Source/Utils/InfiniteParallaxNode.cpp
+++ Source/Utils/InfiniteParallaxNode.cpp
@@ -17,32 +17,11 @@ InfiniteParallaxNode* InfiniteParallaxNode::create()
 	return node;
 }
 
+// TODO: Just put this in an update(float dt) loop
 void InfiniteParallaxNode::updatePosition()
 {
-	// Get visible size
-	Size visibleSize = Director::getInstance()->getVisibleSize();
-
-	// 1. For each child of an parallax node
-	for (int i = 0; i < _children.size(); i++)
+	if (this->getPosition().x < -_children.at(0)->getContentSize().width)
 	{
-		Node* node = _children.at(i);
-
-		float nodeX = convertToWorldSpace(node->getPosition()).x;
-		float width = node->getContentSize().width;
-
-		// 2. We check whether it is out of the left side of the visible area
-		if (nodeX + width < 0)
-
-			// 3. Find PointObject that corresponds to current node
-			for (int i = 0; i < _parallaxArray->num; i++)
-			{
-				PointObject* po = (PointObject*)_parallaxArray->arr[i];
-
-				// If yes increase its current offset on the value of visible width
-				if (po->getChild() == node)
-				{
-					po->setOffset(po->getOffset() + Point(visibleSize.width + node->getContentSize().width, 0));
-				}
-			}
+		this->setPosition(Vec2(0, this->getPosition().y));
 	}
 }
