dedc6980b0611728f783c4b83d9721ad926f3e8f
Zachary Canann <zacharycanann@gmail.com>
2018-01-06 13:36:41 -0800

    Dialog fixes
    Former-commit-id: 281bd3ab117fb2c06368b4ea6d0ed643fd972649 [formerly 281bd3ab117fb2c06368b4ea6d0ed643fd972649 [formerly 75eefb9d0f2a93f1301234e01484f8da4209c4d5]]
    Former-commit-id: 63cece77f5255f86e574632417b6e75625383ff2
    Former-commit-id: 7731dcc18ccdf8a8da98f0dbc81e0c1ba338a469

---
+++ Source/GUI/Components/MenuLabel.cpp
@@ -0,0 +1,85 @@
+#include "MenuLabel.h"
+
+MenuLabel * MenuLabel::create(std::string labelText, std::string fontResource, float fontSize)
+{
+	MenuLabel* menuLabel = new MenuLabel(labelText, fontResource, fontSize);
+
+	menuLabel->autorelease();
+
+	return menuLabel;
+}
+
+MenuLabel::MenuLabel(std::string labelText, std::string fontResource, float fontSize)
+{
+	this->menuOnMouseClick = nullptr;
+
+	this->label = Label::createWithTTF(labelText, fontResource, fontSize);
+	this->labelHighlighted = Label::createWithTTF(labelText, fontResource, fontSize);
+
+	this->label->enableOutline(Color4B::BLACK, fontOutlineSize);
+
+	this->addChild(this->labelHighlighted);
+	this->addChild(this->label);
+
+	this->setContentSize(this->label->getContentSize());
+}
+
+MenuLabel::~MenuLabel()
+{
+}
+
+void MenuLabel::setCallback(std::function<void(MenuLabel*)> callback)
+{
+	this->menuOnMouseClick = callback;
+}
+
+void MenuLabel::setText(std::string text)
+{
+	this->label->setString(text);
+	this->labelHighlighted->setString(text);
+
+	this->setContentSize(this->label->getContentSize());
+}
+
+void MenuLabel::onEnter()
+{
+	Node::onEnter();
+
+	this->initializeListeners();
+}
+
+void MenuLabel::initializeListeners()
+{
+	EventListenerMouse* mouseListener = EventListenerMouse::create();
+
+	mouseListener->onMouseMove = CC_CALLBACK_1(MenuLabel::onMouseMove, this);
+	mouseListener->onMouseDown = CC_CALLBACK_1(MenuLabel::onMouseDown, this);
+
+	this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(mouseListener, this);
+}
+
+void MenuLabel::onMouseMove(EventMouse* event)
+{
+	if (this->menuOnMouseClick != nullptr && this->isVisible())
+	{
+		if (Utils::intersects(this, Vec2(event->getCursorX(), event->getCursorY())))
+		{
+			this->labelHighlighted->enableOutline(*highlightColor, highlightOutlineSize);
+		}
+		else
+		{
+			this->labelHighlighted->enableOutline(Color4B::BLACK, 1);
+		}
+	}
+}
+
+void MenuLabel::onMouseDown(EventMouse* event)
+{
+	if (this->menuOnMouseClick != nullptr && this->isVisible())
+	{
+		if (Utils::intersects(this, Vec2(event->getCursorX(), event->getCursorY())))
+		{
+			this->menuOnMouseClick(this);
+		}
+	}
+}
--- Source/GUI/Menus/Dialog/Dialog.cpp
+++ Source/GUI/Menus/Dialog/Dialog.cpp
@@ -10,37 +10,48 @@ Dialog * Dialog::loadDialogFromJson(std::string json)
 	Document document = Document();
 	document.Parse<0>(json.c_str());
 
-	std::string dialogText = document["Dialog"].GetString();
-	TextMood textMood = Dialog::stringToTextMood(document["TextMood"].GetString());
-	Speaker speaker = Dialog::stringToSpeaker(document["Speaker"].GetString());
-	Portrait portraitLeft = Dialog::stringToPortrait(document["SpeakerLeft"].GetString());
-	Portrait portraitRight = Dialog::stringToPortrait(document["SpeakerRight"].GetString());
-	GenericArray<false, rapidjson::Value::ValueType> children = document["Children"].GetArray();
+	std::string dialogText = document.HasMember("Dialog") ? document["Dialog"].GetString() : "";
+	TextMood textMood = document.HasMember("TextMood") ? Dialog::stringToTextMood(document["TextMood"].GetString()) : TextMood::Normal;
+	Speaker speaker = document.HasMember("Speaker") ? Dialog::stringToSpeaker(document["Speaker"].GetString()) : Speaker::NoSpeaker;
+	Portrait portraitLeft = document.HasMember("PortraitLeft") ? Dialog::stringToPortrait(document["PortraitLeft"].GetString()) : Portrait::NoPortrait;
+	Portrait portraitRight = document.HasMember("PortraitRight") ? Dialog::stringToPortrait(document["PortraitRight"].GetString()) : Portrait::NoPortrait;
 
-	Dialog* dialog = Dialog::create(portraitLeft, portraitRight, speaker, textMood, dialogText);
-	dialog->dialogChoice = document["Choice"].GetString();
+	std::map<std::string, Dialog*>* children = new std::map<std::string, Dialog*>();
 
-	for (rapidjson::Value::ConstValueIterator iterator = children.Begin(); iterator != children.End(); ++iterator)
+	if (document.HasMember("Children"))
 	{
-		Dialog* child = Dialog::loadDialogFromJson(iterator->GetString());
-		dialog->children->insert_or_assign(child->dialogChoice, child);
+		GenericArray<false, rapidjson::Value::ValueType> childrenArr = document["Children"].GetArray();
+		int index = 0;
+
+		for (rapidjson::Value::ConstValueIterator iterator = childrenArr.Begin(); iterator != childrenArr.End(); ++iterator)
+		{
+			rapidjson::StringBuffer stringBuffer;
+			rapidjson::Writer<rapidjson::StringBuffer> writer(stringBuffer);
+			document["Children"][index].Accept(writer);
+
+			Dialog* child = Dialog::loadDialogFromJson(stringBuffer.GetString());
+			children->insert_or_assign(child->dialogChoice, child);
+			index++;
+		}
 	}
+
+	Dialog* dialog = Dialog::create(portraitLeft, portraitRight, speaker, textMood, dialogText, children);
+	dialog->dialogChoice = document["Choice"].GetString();
 
 	return dialog;
 }
 
-Dialog* Dialog::create(Portrait portraitLeft, Portrait portraitRight, Speaker speaker, TextMood textMood, std::string text)
+Dialog* Dialog::create(Portrait portraitLeft, Portrait portraitRight, Speaker speaker, TextMood textMood, std::string text, std::map<std::string, Dialog*>* children)
 {
-	Dialog* dialog = new Dialog(portraitLeft, portraitRight, speaker, textMood, text);
+	Dialog* dialog = new Dialog(portraitLeft, portraitRight, speaker, textMood, text, children);
 
 	dialog->autorelease();
 
 	return dialog;
 }
 
-Dialog::Dialog(Portrait portraitLeft, Portrait portraitRight, Speaker speaker, TextMood textMood, std::string text)
+Dialog::Dialog(Portrait portraitLeft, Portrait portraitRight, Speaker speaker, TextMood textMood, std::string text, std::map<std::string, Dialog*>* children)
 {
-	this->children = new std::map<std::string, Dialog*>();
 	this->spriteLeft = this->getPortraitNode(portraitLeft, false);
 	this->spriteRight = this->getPortraitNode(portraitRight, true);
 
@@ -86,6 +97,18 @@ Dialog::Dialog(Portrait portraitLeft, Portrait portraitRight, Speaker speaker, T
 		break;
 	}
 
+	this->dialogChildren = new std::map<MenuLabel*, Dialog*>();
+
+	for (auto iterator = children->begin(); iterator != children->end(); ++iterator)
+	{
+		MenuLabel* label = MenuLabel::create(iterator->first, Resources::Fonts_Montserrat_Medium, 24);
+		label->setCallback(CC_CALLBACK_1(Dialog::onChooseDialog, this));
+
+		this->dialogChildren->insert_or_assign(label, iterator->second);
+	}
+
+	delete(children);
+
 	this->initializePositions();
 
 	this->spriteLeftClip->addChild(this->spriteLeft);
@@ -93,20 +116,66 @@ Dialog::Dialog(Portrait portraitLeft, Portrait portraitRight, Speaker speaker, T
 	this->addChild(this->spriteLeftClip);
 	this->addChild(this->spriteRightClip);
 	this->addChild(this->dialogText);
+
+	for (auto iterator = this->dialogChildren->begin(); iterator != this->dialogChildren->end(); ++iterator)
+	{
+		this->addChild(iterator->first);
+	}
 }
 
 Dialog::~Dialog()
 {
-	delete(this->children);
+	delete(this->dialogChildren);
 }
 
 void Dialog::initializePositions()
 {
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 
-	this->spriteLeftClip->setPosition(Vec2(visibleSize.width / 2 - 732, 16));
-	this->spriteRightClip->setPosition(Vec2(visibleSize.width / 2 + 732, 16));
-	this->dialogText->setPosition(Vec2(visibleSize.width / 2, -16));
+	this->spriteLeftClip->setPosition(Vec2(-732.0f, 16.0f));
+	this->spriteRightClip->setPosition(Vec2(+732.0f, 16.0f));
+	this->dialogText->setPosition(Vec2(0.0f, -16.0f));
+
+	int index = 0;
+	int count = (int)this->dialogChildren->size();
+
+	for (auto iterator = this->dialogChildren->begin(); iterator != this->dialogChildren->end(); ++iterator)
+	{
+		Vec2 pos = Vec2(0.0f, 0.0f);
+
+		switch (count)
+		{
+		case 1:
+			switch (index)
+			{
+			case 0:
+				pos.y -= 64.0f;
+				break;
+			}
+		case 2:
+			switch (index)
+			{
+			case 0:
+				pos.x -= 256.0f;
+				pos.y -= 64.0f;
+				break;
+			case 1:
+				pos.x += 256.0f;
+				pos.y -= 64.0f;
+				break;
+			}
+		default:
+			break;
+		}
+
+		iterator->first->setPosition(pos);
+		index++;
+	}
+}
+
+void Dialog::onChooseDialog(MenuLabel* dialogMenu)
+{
+
 }
 
 Node* Dialog::getPortraitNode(Portrait portrait, bool isRight)
