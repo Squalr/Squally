1d850d39444aa7d124a387585eedd8a59cccdd68
Zachary Canann <zacharycanann@gmail.com>
2018-01-24 23:00:21 -0800

    Mouse over panel updates, beginning code editor
    Former-commit-id: b5e410944f1913393cf8cbbdf3d555183a3521bb [formerly b5e410944f1913393cf8cbbdf3d555183a3521bb [formerly d8b42a37700e2c0a1c03273d75fcc9b56b8c3ced]]
    Former-commit-id: 45bfbf9748dfcf81abf8136612dc3107ba1bcfbb
    Former-commit-id: 6ce17167192a21d1690f6019e9034b5be0dd8240

--- Source/GUI/Components/MouseOverPanel.h
+++ Source/GUI/Components/MouseOverPanel.h
@@ -8,16 +8,20 @@ using namespace cocos2d;
 class MouseOverPanel : public Node
 {
 public:
-	static MouseOverPanel * create(Node* visibleContent, Node* mouseOverContent, Vec2 size);
+	static MouseOverPanel * create(Node* visibleContent, Node* mouseOverContent, Node* panelParent, Size size);
 
 private:
-	MouseOverPanel(Node* visibleContent, Node* mouseOverContent, Vec2 size);
+	MouseOverPanel(Node* visibleContent, Node* mouseOverContent, Node* panelParent, Size size);
 	~MouseOverPanel();
 
 	void initializeListeners();
 	void onMouseSpriteMove(EventCustom* args);
 
-	Sprite* panelBackground;
+	LayerColor* panelBackgroundFrame;
+	LayerColor* panelBackground;
 	Node* content;
 	Node* panelContent;
+	Node* panelParentHost;
+
+	const int frameWidth = 4;
 };
