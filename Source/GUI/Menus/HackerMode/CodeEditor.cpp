a5a94ee98d8b08916f48c3bf7f1cfe03bff87ecc
Zachary Canann <zacharycanann@gmail.com>
2018-01-27 15:59:22 -0800

    Using text field for code editor
    Former-commit-id: f43a35acba2a77b8a7015ff715d3891530c4693c [formerly f43a35acba2a77b8a7015ff715d3891530c4693c [formerly db93af61a45ccb902dba48911c9d308d27cd6d0b]]
    Former-commit-id: ace1e507ffc994c089979054c125be2d1f2af4ee
    Former-commit-id: e98a4cd73d998b304e53a8c6464df11fb136aa22

--- Source/GUI/Menus/HackerMode/CodeEditor.cpp
+++ Source/GUI/Menus/HackerMode/CodeEditor.cpp
@@ -16,20 +16,15 @@ CodeEditor::CodeEditor()
 	this->displayTextElements = new std::vector<RichElementText*>();
 	this->codeEditorBackground = Sprite::create(Resources::Menus_HackerModeMenu_CodeEditMenu);
 	this->closeButton = MenuSprite::create(Resources::Menus_HackerModeMenu_CloseButton, Resources::Menus_HackerModeMenu_CloseButtonHover, Resources::Menus_HackerModeMenu_CloseButtonClick);
-	this->assemblyCodeText = EditBox::create(CodeEditor::textSize, Resources::EmptyImage);
+	this->assemblyCodeText = TextField::create("<Click to Edit Assembly Code>", Resources::Fonts_Montserrat_Medium, 24);
 	this->displayText = RichText::create();
 
-	this->assemblyCodeText->setFontName(Resources::Fonts_Montserrat_Medium.c_str());
-	this->assemblyCodeText->setFontSize(24);
-	this->assemblyCodeText->setFontColor(Color3B::WHITE);
-	this->assemblyCodeText->setPlaceHolder("<Assembly code here>");
-	this->assemblyCodeText->setPlaceholderFontColor(Color3B::GRAY);
-	this->assemblyCodeText->setPlaceholderFontSize(24);
-	this->assemblyCodeText->setInputMode(EditBox::InputMode::ANY);
-	this->assemblyCodeText->setInputFlag(EditBox::InputFlag::SENSITIVE);
 	this->assemblyCodeText->setOpacity(0);
 	this->assemblyCodeText->setCascadeOpacityEnabled(true);
+	this->assemblyCodeText->setCursorEnabled(true);
+	this->assemblyCodeText->setHighlighted(true);
 
+	this->assemblyCodeText->setAnchorPoint(Vec2(0.0f, 1.0f));
 	this->displayText->setAnchorPoint(Vec2(0.0f, 1.0f));
 
 	this->addChild(this->codeEditorBackground);
@@ -49,22 +44,37 @@ CodeEditor::~CodeEditor()
 
 void CodeEditor::update(float dt)
 {
-	if (this->previousAssemblyText != this->assemblyCodeText->getText())
+	std::string currentText = this->assemblyCodeText->getString();
+
+	if (this->previousAssemblyText == currentText)
 	{
-		this->previousAssemblyText = this->assemblyCodeText->getText();
-
-		for (auto iterator = this->displayTextElements->begin(); iterator != this->displayTextElements->end(); iterator++)
-		{
-			this->displayText->removeElement(*iterator);
-		}
-
-		this->displayTextElements->clear();
-
-		// Reconstruct text
-		RichElementText* element = RichElementText::create(0, Color3B::WHITE, 255, this->assemblyCodeText->getText(), Resources::Fonts_Montserrat_Medium, 24);
-		this->displayTextElements->push_back(element);
-		this->displayText->pushBackElement(element);
+		return;
 	}
+
+	this->previousAssemblyText = currentText;
+
+	// Set visibility of placeholder text
+	if (std::all_of(currentText.begin(), currentText.end(), iswspace))
+	{
+		this->assemblyCodeText->setOpacity(0xFF);
+	}
+	else
+	{
+		this->assemblyCodeText->setOpacity(0);
+	}
+
+	// Remove existing rich text
+	for (auto iterator = this->displayTextElements->begin(); iterator != this->displayTextElements->end(); iterator++)
+	{
+		this->displayText->removeElement(*iterator);
+	}
+
+	this->displayTextElements->clear();
+
+	// Reconstruct rich text
+	RichElementText* element = RichElementText::create(0, Color3B::WHITE, 255, currentText, Resources::Fonts_Montserrat_Medium, 24);
+	this->displayTextElements->push_back(element);
+	this->displayText->pushBackElement(element);
 }
 
 void CodeEditor::initializePositions()
@@ -73,7 +83,7 @@ void CodeEditor::initializePositions()
 
 	this->codeEditorBackground->setPosition(Vec2(visibleSize.width / 2.0f, visibleSize.height / 2.0f));
 	this->closeButton->setPosition(Vec2(visibleSize.width / 2.0f + 612.0f, visibleSize.height / 2.0f + 336.0f));
-	this->assemblyCodeText->setPosition(Vec2(visibleSize.width / 2.0f, visibleSize.height / 2.0f));
+	this->assemblyCodeText->setPosition(Vec2(visibleSize.width / 2.0f - CodeEditor::textSize.width / 2, visibleSize.height / 2.0f + CodeEditor::textSize.height / 2));
 	this->displayText->setPosition(Vec2(visibleSize.width / 2.0f - CodeEditor::textSize.width / 2, visibleSize.height / 2.0f + CodeEditor::textSize.height / 2));
 }
 
@@ -85,6 +95,8 @@ void CodeEditor::initializeListeners()
 void CodeEditor::open(HackableCode* hackableCode)
 {
 	this->assemblyCodeText->setText(HackUtils::disassemble(hackableCode->codePointer, hackableCode->codeOriginalLength).c_str());
+
+	this->assemblyCodeText->attachWithIME();
 
 	this->setVisible(true);
 	Utils::focus(this);
