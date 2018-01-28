a35b48a902e399a3f55d0bc0a0ec6a9e42ed1ebe
Zachary Canann <zacharycanann@gmail.com>
2018-01-28 13:59:39 -0800

    Syntax highlighting for all registers, rich text box fixes

--- Source/Utils/StrUtils.h
+++ Source/Utils/StrUtils.h
@@ -8,6 +8,7 @@ class StrUtils
 {
 public:
 	static std::vector<std::string>* tokenize(std::string str, std::string delimiters);
+	static std::vector<std::string>* splitOn(std::string str, std::string delimiter);
 	static std::string replaceAll(std::string str, const std::string& from, const std::string& to);
 };
 
