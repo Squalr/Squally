1792f78d726b4a99e8f7b8f03d349d662558b5cd
Zachary Canann <zacharycanann@gmail.com>
2018-02-04 22:36:28 -0800

    Code menu visual updates
    Former-commit-id: cf3ed0b63d7748d9d4619950b7343a50d9709e9a [formerly cf3ed0b63d7748d9d4619950b7343a50d9709e9a [formerly 9eae90d28849cc299effe92c016b3eec77df41b5]]
    Former-commit-id: 3c39b9584feaebf9b2c8c4b67a2ce6c0d38a8848
    Former-commit-id: ae489c583283cf1473d0cd74ecafbc10620ad088

--- Source/GUI/Menus/HackerMode/CodeEditor.cpp
+++ Source/GUI/Menus/HackerMode/CodeEditor.cpp
@@ -65,9 +65,9 @@ CodeEditor::CodeEditor()
 	this->statusWindow = TextWindow::create("Status", CodeEditor::statusSize, 32, CodeEditor::defaultColor);
 	this->functionWindow = EditableTextWindow::create("Code Editor", CodeEditor::functionSize, 32, CodeEditor::defaultColor);
 	this->secondaryWindow = EditableTextWindow::create("Allocation Editor", CodeEditor::secondarySize, 32, CodeEditor::defaultColor);
-	this->cancelButton = MenuSprite::create(Resources::Menus_HackerModeMenu_CancelButton, Resources::Menus_HackerModeMenu_CancelButtonHover, Resources::Menus_HackerModeMenu_CancelButtonClick);
-	this->acceptButton = MenuSprite::create(Resources::Menus_HackerModeMenu_AcceptButton, Resources::Menus_HackerModeMenu_AcceptButtonHover, Resources::Menus_HackerModeMenu_AcceptButtonClick);
-	this->acceptButtonGrayed = Sprite::create(Resources::Menus_HackerModeMenu_AcceptButtonGray);
+	this->cancelButton = MenuSprite::create(Resources::Menus_HackerModeMenu_CancelTextButton, Resources::Menus_HackerModeMenu_CancelTextButtonHover, Resources::Menus_HackerModeMenu_CancelTextButtonClick);
+	this->acceptButton = MenuSprite::create(Resources::Menus_HackerModeMenu_AcceptTextButton, Resources::Menus_HackerModeMenu_AcceptTextButtonHover, Resources::Menus_HackerModeMenu_AcceptTextButtonClick);
+	this->acceptButtonGrayed = Sprite::create(Resources::Menus_HackerModeMenu_AcceptTextButtonGray);
 
 	this->functionWindow->setTokenizationCallback(CC_CALLBACK_2(CodeEditor::tokenizeCallback, this));
 	this->secondaryWindow->setTokenizationCallback(CC_CALLBACK_2(CodeEditor::tokenizeCallback, this));
@@ -100,8 +100,8 @@ void CodeEditor::initializePositions()
 
 	this->codeEditorBackground->setPosition(Vec2(visibleSize.width / 2.0f, visibleSize.height / 2.0f));
 	this->codeEditorTitle->setPosition(Vec2(visibleSize.width / 2.0f, visibleSize.height / 2.0f + 444.0f));
-	this->acceptButton->setPosition(Vec2(visibleSize.width / 2.0f + 224.0f, visibleSize.height / 2.0f - 432.0f));
-	this->cancelButton->setPosition(Vec2(visibleSize.width / 2.0f - 224.0f, visibleSize.height / 2.0f - 432.0f));
+	this->acceptButton->setPosition(Vec2(visibleSize.width / 2.0f + 512.0f, visibleSize.height / 2.0f - 432.0f));
+	this->cancelButton->setPosition(Vec2(visibleSize.width / 2.0f - 512.0f, visibleSize.height / 2.0f - 432.0f));
 	this->acceptButtonGrayed->setPosition(this->acceptButton->getPosition());
 
 	this->statusWindow->setPosition(Vec2(visibleSize.width / 2.0f - 560.0f, visibleSize.height / 2.0f - 64.0f));
