ad59d785b78e0769c9d4664dc2448a8d9a58e6d1
Zachary Canann <zacharycanann@gmail.com>
2018-02-22 00:09:10 -0800

    Mini game rename
    Former-commit-id: 6f25209b3b8fcb7656c5ed5c4ced5f29969aac49 [formerly 6f25209b3b8fcb7656c5ed5c4ced5f29969aac49 [formerly b1ba665d2c4e5f41b26a2ade1945c22b9fdbf9b0]]
    Former-commit-id: b2377a48b4cf821e403e7f7ce56a4b2f6c1b25d0
    Former-commit-id: 75f61f3d8a1a606747d99859335ed2902f77ea3c

--- Source/Entities/Npcs/Skeleton.cpp
+++ Source/Entities/Npcs/Skeleton.cpp
@@ -9,7 +9,7 @@ Skeleton* Skeleton::create()
 	return skeleton;
 }
 
-Skeleton::Skeleton() : Entity::Entity()
+Skeleton::Skeleton() : NpcBase::NpcBase()
 {
 	this->actualJumpLaunchVelocity = 640.0f;
 	this->actualGravityAcceleration = 1000.0f;
@@ -20,14 +20,7 @@ Skeleton::Skeleton() : Entity::Entity()
 	this->movement.y = 0.0f;
 
 	this->spriteNode = Node::create();
-	this->skeletonSprite = Sprite::create(Resources::Ingame_Sprites_Snail_SnailWalking1);
-	this->walkAnimation = Animation::create();
-	this->walkAnimation->addSpriteFrameWithFileName(Resources::Ingame_Sprites_Snail_SnailWalking1);
-	this->walkAnimation->addSpriteFrameWithFileName(Resources::Ingame_Sprites_Snail_SnailWalking2);
-	this->walkAnimation->addSpriteFrameWithFileName(Resources::Ingame_Sprites_Snail_SnailWalking3);
-	this->walkAnimation->addSpriteFrameWithFileName(Resources::Ingame_Sprites_Snail_SnailWalking4);
-	this->walkAnimation->setDelayPerUnit(0.15f);
-	this->skeletonSprite->runAction(RepeatForever::create(Sequence::create(Animate::create(this->walkAnimation), nullptr)));
+	this->skeletonSprite = Sprite::create(Resources::Ingame_Sprites_Knight_KnightStanding);
 
 	this->init(PhysicsBody::createBox(this->skeletonSprite->getContentSize()), CategoryGroup::G_Npc, true, false);
 
@@ -41,76 +34,5 @@ Skeleton::~Skeleton()
 
 void Skeleton::update(float dt)
 {
-	Entity::update(dt);
-
-	this->movement.y = 0.0f;
-
-	/*
-	if (this->getRotation() < -15.0f)
-	{
-		this->setRotation(-15.0f);
-	}
-	else if (this->getRotation() > 15.0f)
-	{
-		this->setRotation(15.0f);
-	}*/
-
-	if (this->movement.x < 0.0f)
-	{
-		this->skeletonSprite->setFlippedX(true);
-	}
-	else
-	{
-		this->skeletonSprite->setFlippedX(false);
-	}
-}
-
-bool Skeleton::contactBegin(CollisionData data)
-{
-	return false;
-}
-
-bool Skeleton::contactUpdate(CollisionData data)
-{
-	switch (data.other->getCategoryGroup())
-	{
-	case CategoryGroup::G_SolidNpc:
-	case CategoryGroup::G_SolidFlyingNpc:
-	case CategoryGroup::G_Solid:
-		if (abs(data.normal.y) >= Entity::normalJumpThreshold)
-		{
-			this->isOnGround = true;
-		}
-
-		switch (data.direction)
-		{
-		case CollisionDirection::Left:
-			this->movement.x = 1.0f;
-			break;
-		case CollisionDirection::Right:
-			this->movement.x = -1.0f;
-			break;
-		case CollisionDirection::StepLeft:
-		case CollisionDirection::StepRight:
-			this->movement.y = 0.35f;
-			break;
-		}
-		return true;
-	}
-
-	return false;
-}
-
-bool Skeleton::contactEnd(CollisionData data)
-{
-	switch (data.other->getCategoryGroup())
-	{
-	case CategoryGroup::G_Solid:
-	case CategoryGroup::G_SolidNpc:
-	case CategoryGroup::G_SolidFlyingNpc:
-		this->isOnGround = false;
-		return true;
-	}
-
-	return false;
+	NpcBase::update(dt);
 }
