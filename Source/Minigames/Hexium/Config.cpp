80c8cc0a576f109e2acd31c1ca4c6de34fa526f0
Zachary Canann <zacharycanann@gmail.com>
2018-03-02 23:33:12 -0800

    Organizational updates to card game
    Former-commit-id: 5ac4fefdd05c317bcbd1d282bb103de17d17912b [formerly 5ac4fefdd05c317bcbd1d282bb103de17d17912b [formerly f0e916ae26b67f77092ab101add5da9d2763efd7]]
    Former-commit-id: b38341f33e17ad609b0a370dc82b6bf8f7019ca6
    Former-commit-id: adcdd60bf4ef5c3d9c2c0c9c7aa718feed02c2a0

---
+++ Source/Minigames/Hexium/Config.cpp
@@ -0,0 +1,23 @@
+#include "Config.h"
+
+const float Config::rightColumnCenter = 780.0f;
+const float Config::leftColumnCenter = -712.0f;
+const float Config::centerColumnCenter = 68.0f;
+const float Config::emblemOffsetX = -84.0f;
+const float Config::frameOffsetX = -72.0f;
+const float Config::frameOffsetY = 368.0f;
+const float Config::deckOffsetX = 64.0f;
+const float Config::deckOffsetY = 420.0f;
+const float Config::handOffsetY = 472.0f;
+const float Config::boardCenterOffsetY = 72.0f;
+const float Config::binaryRowOffsetY = 72.0f;
+const float Config::decimalRowOffsetY = 228.0f;
+const float Config::hexRowOffsetY = 384.0f;
+const float Config::labelsOffsetY = 72.0f;
+const float Config::previewOffsetY = 64.0f;
+const float Config::graveyardOffsetX = -64.0f;
+const float Config::graveyardOffsetY = Config::deckOffsetY;
+const float Config::handCardCountOffsetX = 196.0f;
+const float Config::socketOffsetY = 236.0f;
+const float Config::socketAOffsetX = 64.0f;
+const float Config::socketBOffsetX = Config::socketAOffsetX + 112.0f;
---
+++ Source/Minigames/Hexium/GameState.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "cocos2d.h"
+#include "Resources.h"
+#include "Minigames/Hexium/CardRow.h"
+#include "Minigames/Hexium/Deck.h"
+#include "MiniGames/Hexium/Config.h"
+#include "Utils/StrUtils.h"
+
+using namespace cocos2d;
+
+class GameState : public Node
+{
+public:
+	static GameState * create();
+
+	void gameStart(Deck* startPlayerDeck, Deck* startEnemyDeck);
+	void randomizeTurn();
+	void drawCard();
+	void giveControl();
+	void endTurn();
+	void setCardPreviewCallback(std::function<void(Card*)> callback);
+	void setUpdateStateCallback(std::function<void(bool)> callback);
+
+	enum Turn {
+		Player,
+		Enemy,
+	};
+
+	// Game state
+	Turn turn;
+	int playerLosses;
+	int enemyLosses;
+
+	Deck* playerDeck;
+	CardRow* playerHand;
+	Deck* playerGraveyard;
+	CardRow* playerBinaryCards;
+	CardRow* playerDecimalCards;
+	CardRow* playerHexCards;
+
+	Deck* enemyDeck;
+	CardRow* enemyHand;
+	Deck* enemyGraveyard;
+	CardRow* enemyBinaryCards;
+	CardRow* enemyDecimalCards;
+	CardRow* enemyHexCards;
+
+private:
+	GameState();
+	~GameState();
+	void onEnter() override;
+	void onCardMouseOver(Card* card);
+	void onHandCardClick(Card* card);
+	void onRowCardClick(Card* card);
+
+	void initializePositions();
+	void initializeListeners();
+
+	std::function<void(Card*)> cardPreviewCallback;
+	std::function<void(bool)> updateStateCallback;
+};
+
