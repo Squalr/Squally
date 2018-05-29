64b6eea57a334dcf11c16208a396d500f069f7c1
Matthew Bucci <mtbucci@gmail.com>
2018-05-28 23:44:24 -0700

    Move Windows specific files into proj.windows folder in prepration for cross compatibility

--- Source/main.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
-
-#include <windows.h>			// Windows header files
-#include <tchar.h>				// Windows header files
-#include "platform/CCStdC.h"	// C RunTime header files
-#include "Window.h"
-#include "cocos2d.h"
