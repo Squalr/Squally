74f87a25a69c22565cb0a8dc7fe95b10018caccd
Zachary Canann <zacharycanann@gmail.com>
2018-06-04 00:39:01 -0700

    Level parser updates

---
+++ Source/Level/Parser/Keys/EnvironmentKeys.cpp
@@ -0,0 +1,13 @@
+#include "EnvironmentKeys.h"
+
+const std::string EnvironmentKeys::BackgroundProperty = "background";
+const std::string EnvironmentKeys::BackgroundJungle = "jungle";
+const std::string EnvironmentKeys::BackgroundSnow = "snow";
+const std::string EnvironmentKeys::BackgroundOcean = "ocean";
+const std::string EnvironmentKeys::BackgroundMountain = "mountain";
+
+const std::string EnvironmentKeys::WeatherProperty = "weather";
+const std::string EnvironmentKeys::WeatherSnow = "snow";
+const std::string EnvironmentKeys::WeatherRain = "rain";
+
+const std::string EnvironmentKeys::MusicProperty = "music";
---
+++ Source/Level/Parser/Keys/GeneralKeys.cpp
@@ -0,0 +1,8 @@
+#include "GeneralKeys.h"
+
+const std::string GeneralKeys::Type = "type";
+const std::string GeneralKeys::Width = "width";
+const std::string GeneralKeys::Height = "height";
+const std::string GeneralKeys::XPosition = "x";
+const std::string GeneralKeys::YPosition = "y";
+const std::string GeneralKeys::Rotation = "rotation";
