e5b40a7628883326eba717dc7f95981777ac7712
Zachary Canann <zacharycanann@gmail.com>
2018-09-04 18:52:33 -0700

    Cutscenes cutscenes cutscenes cutscenes

--- Source/Scenes/Cutscenes/IntroSpace.cpp
+++ Source/Scenes/Cutscenes/IntroSpace.cpp
@@ -1,5 +1,11 @@
 #include "IntroSpace.h"
 
+const float IntroSpace::dialogueHeight = 256.0f;
+
+const float IntroSpace::marsDelta = 1280.0f;
+const float IntroSpace::weaverDelta = 1280.0f;// IntroSpace::marsDelta - 512.0f;
+const float IntroSpace::earthDelta = 1280.0f;// IntroSpace::weaverDelta - 1024.0f;
+
 IntroSpace* IntroSpace::create()
 {
 	IntroSpace* instance = new IntroSpace();
@@ -16,6 +22,7 @@ IntroSpace::IntroSpace()
 	this->starLayer = StarLayer::create(Size(visibleSize.width * 2.0f, visibleSize.height));
 	this->earth = Sprite::create(Resources::Cutscenes_IntroSpace_Earth);
 	this->mars = Sprite::create(Resources::Cutscenes_IntroSpace_Mars);
+	this->weavers = Node::create();
 	this->weaver1 = Sprite::create();
 	this->weaver2 = Sprite::create();
 	this->weaver3 = Sprite::create();
@@ -26,7 +33,11 @@ IntroSpace::IntroSpace()
 	this->weaver3Anim = Animation::create();
 	this->weaver4Anim = Animation::create();
 	this->weaver5Anim = Animation::create();
+	this->dialoguePlate = LayerColor::create(Color4B(0, 0, 0, 196), visibleSize.width, IntroSpace::dialogueHeight);
+	this->dialogue = Dialogue::create(Resources::Strings_Dialogue_CutsceneIntroSpace, Localization::getPixelFont(), Size(visibleSize.width - 48.0f, 256.0f - 48.0f));
+	this->escapeLabel = Label::create("Press esc to skip", Localization::getPixelFont(), 20.0f, Size::ZERO, TextHAlignment::LEFT);
 
+	this->escapeLabel->setAnchorPoint(Vec2(1.0f, 0.5f));
 	this->weaver1->setScale(0.12);
 	this->weaver2->setScale(0.14);
 	this->weaver3->setScale(0.16);
@@ -44,21 +55,19 @@ IntroSpace::IntroSpace()
 		this->weaver5Anim->addSpriteFrameWithFileName(*it);
 	}
 
-	this->weaver1->retain();
-	this->weaver2->retain();
-	this->weaver3->retain();
-	this->weaver4->retain();
-	this->weaver5->retain();
-
 	this->addChild(InputManager::claimInstance());
 	this->addChild(this->starLayer);
 	this->addChild(this->earth);
 	this->addChild(this->mars);
-	this->addChild(this->weaver1);
-	this->addChild(this->weaver2);
-	this->addChild(this->weaver3);
-	this->addChild(this->weaver4);
-	this->addChild(this->weaver5);
+	this->weavers->addChild(this->weaver1);
+	this->weavers->addChild(this->weaver2);
+	this->weavers->addChild(this->weaver3);
+	this->weavers->addChild(this->weaver4);
+	this->weavers->addChild(this->weaver5);
+	this->addChild(this->weavers);
+	this->addChild(this->dialoguePlate);
+	this->addChild(this->dialogue);
+	this->addChild(this->escapeLabel);
 }
 
 IntroSpace::~IntroSpace()
@@ -84,6 +93,8 @@ void IntroSpace::onEnter()
 	this->scheduleUpdate();
 	this->initializePositions();
 	this->initializeListeners();
+
+	this->runCutscene();
 }
 
 void IntroSpace::initializePositions()
@@ -91,20 +102,27 @@ void IntroSpace::initializePositions()
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 
 	this->mars->setAnchorPoint(Vec2(0.0f, 0.0f));
-	this->mars->setPosition(Vec2(64.0f, 48.0f));
+	this->mars->setPosition(Vec2(64.0f + marsDelta, 48.0f));
 	this->earth->setAnchorPoint(Vec2(1.0f, 1.0f));
-	this->earth->setPosition(Vec2(visibleSize.width - 32.0f, visibleSize.height - 32.0f));
+	this->earth->setPosition(Vec2(visibleSize.width - 32.0f + earthDelta, visibleSize.height - 32.0f));
+	this->weavers->setPositionX(weaverDelta);
 
 	this->weaver1->setPosition(Vec2(visibleSize.width / 2.0f + 432.0f, visibleSize.height / 2.0f + 240.0f));
 	this->weaver2->setPosition(Vec2(visibleSize.width / 2.0f + 384.0f, visibleSize.height / 2.0f + 224.0f));
 	this->weaver3->setPosition(Vec2(visibleSize.width / 2.0f + 412.0f, visibleSize.height / 2.0f + 196.0f));
 	this->weaver4->setPosition(Vec2(visibleSize.width / 2.0f + 368.0f, visibleSize.height / 2.0f + 188.0f));
 	this->weaver5->setPosition(Vec2(visibleSize.width / 2.0f + 320.0f, visibleSize.height / 2.0f + 160.0f));
+
+	this->dialoguePlate->setPosition(Vec2(visibleSize.width / 2.0f - this->dialoguePlate->getContentSize().width / 2.0f, 0.0f));
+	this->dialogue->setPosition(Vec2(24.0f, this->dialoguePlate->getContentSize().height - 24.0f));
+	this->escapeLabel->setPosition(Vec2(visibleSize.width - 24.0f, 24.0f));
 }
 
 void IntroSpace::initializeListeners()
 {
 	this->getEventDispatcher()->removeEventListenersForTarget(this);
+
+	this->dialogue->setDialogueShownCallback(CC_CALLBACK_0(IntroSpace::onDialogueShown, this));
 }
 
 void IntroSpace::update(float dt)
@@ -116,6 +134,39 @@ void IntroSpace::update(float dt)
 		this->endCutscene();
 	}
 }
+
+void IntroSpace::onDialogueShown()
+{
+	this->dialogue->runAction(Sequence::create(
+		DelayTime::create(3.0f),
+		CallFunc::create([=]() {
+			if (!this->dialogue->showNextDialogue())
+			{
+			}
+		}),
+		nullptr
+		));
+}
+
+void IntroSpace::runCutscene()
+{
+	float duration = 5.0f;
+
+	this->dialogue->showNextDialogue();
+
+	CallFunc* scroll = CallFunc::create([=]() {
+		this->earth->runAction(MoveTo::create(duration, this->earth->getPosition() - Vec2(IntroSpace::earthDelta, 0.0f)));
+		this->weavers->runAction(MoveTo::create(duration, this->weavers->getPosition() - Vec2(IntroSpace::weaverDelta, 0.0f)));
+		this->mars->runAction(MoveTo::create(duration, this->mars->getPosition() - Vec2(IntroSpace::marsDelta, 0.0f)));
+	});
+
+	this->runAction(Sequence::create(
+		DelayTime::create(3.0f),
+		scroll,
+		nullptr
+	));
+}
+
 
 void IntroSpace::endCutscene()
 {
