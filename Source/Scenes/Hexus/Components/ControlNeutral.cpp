afbfd05ba43b33814d9eef14e5780fc14013b578
Matthew Bucci <buccim2@gene.com>
2018-09-20 23:53:39 -0700

    add AI pass logic

--- Source/Scenes/Hexus/Components/ControlNeutral.cpp
+++ Source/Scenes/Hexus/Components/ControlNeutral.cpp
@@ -54,59 +54,64 @@ void ControlNeutral::selectCard(Card* card)
 void ControlNeutral::aiDoSelection(GameState* gameState)
 {
 	bool selectionMade = false;
+	int passIfDiffAbove = 30;
 
 	this->activeGameState->selectedCard = nullptr;
 
-	for (auto it = gameState->enemyHand->rowCards->begin(); it != gameState->enemyHand->rowCards->end(); it++)
-	{
-		Card* card = *it;
-		std::vector<CardRow *> rows = gameState->getAllRows();
-
-		switch (card->cardData->cardType)
+	if (gameState->getPlayerTotal() > gameState->getEnemyTotal() + passIfDiffAbove || gameState->playerPass && gameState->getEnemyTotal() > gameState->getPlayerTotal()) {
+		gameState->enemyPass = true;
+	} else {
+		for (auto it = gameState->enemyHand->rowCards->begin(); it != gameState->enemyHand->rowCards->end(); it++)
 		{
-		case CardData::CardType::Binary:
-		case CardData::CardType::Decimal:
-		case CardData::CardType::Hexidecimal:
-			this->activeGameState->selectedCard = card;
-			selectionMade = true;
-			break;
-		case CardData::CardType::Special_SHL:
-		case CardData::CardType::Special_SHR:
-		case CardData::CardType::Special_FLIP1:
-		case CardData::CardType::Special_FLIP2:
-		case CardData::CardType::Special_FLIP3:
-		case CardData::CardType::Special_FLIP4:
-		case CardData::CardType::Special_INV: {
-			// don't play this card if there are no viable moves
-			Card::Operation operation = Card::toOperation(card->cardData->cardType, 0);
-			for (auto it = rows.begin(); it != rows.end(); it++)
+			Card* card = *it;
+			std::vector<CardRow *> rows = gameState->getAllRows();
+
+			switch (card->cardData->cardType)
 			{
-				CardRow* row = *it;
-				int diff = row->simulateCardEffect(card) * (row->isPlayerRow() ? -1 : 1);
-				if (diff > 1) {
-					this->activeGameState->selectedCard = card;
-					selectionMade = true;
+			case CardData::CardType::Binary:
+			case CardData::CardType::Decimal:
+			case CardData::CardType::Hexidecimal:
+				this->activeGameState->selectedCard = card;
+				selectionMade = true;
+				break;
+			case CardData::CardType::Special_SHL:
+			case CardData::CardType::Special_SHR:
+			case CardData::CardType::Special_FLIP1:
+			case CardData::CardType::Special_FLIP2:
+			case CardData::CardType::Special_FLIP3:
+			case CardData::CardType::Special_FLIP4:
+			case CardData::CardType::Special_INV: {
+				// don't play this card if there are no viable moves
+				Card::Operation operation = Card::toOperation(card->cardData->cardType, 0);
+				for (auto it = rows.begin(); it != rows.end(); it++)
+				{
+					CardRow* row = *it;
+					int diff = row->simulateCardEffect(card) * (row->isPlayerRow() ? -1 : 1);
+					if (diff > 1) {
+						this->activeGameState->selectedCard = card;
+						selectionMade = true;
+					}
 				}
+				break;
 			}
-			break;
-		}
-		/*
-		case CardData::CardType::Special_AND:
-		case CardData::CardType::Special_OR:
-		case CardData::CardType::Special_XOR:
-		case CardData::CardType::Special_ADD:
-		case CardData::CardType::Special_SUB:
-			this->activeGameState->selectedCard = card;
-			selectionMade = true;
-			break;
-		*/
-		default: 
-			break;
-		}
+			/*
+			case CardData::CardType::Special_AND:
+			case CardData::CardType::Special_OR:
+			case CardData::CardType::Special_XOR:
+			case CardData::CardType::Special_ADD:
+			case CardData::CardType::Special_SUB:
+				this->activeGameState->selectedCard = card;
+				selectionMade = true;
+				break;
+			*/
+			default: 
+				break;
+			}
 
-		if (selectionMade)
-		{
-			break;
+			if (selectionMade)
+			{
+				break;
+			}
 		}
 	}
 
