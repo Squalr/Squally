468e0550113454d032c82c93d8b6607c08c41ea0
Matthew Bucci <buccim2@gene.com>
2018-09-27 22:49:35 -0700

    Add button to skip drawing cards

--- Source/Scenes/Hexus/Components/ControlReplaceCards.cpp
+++ Source/Scenes/Hexus/Components/ControlReplaceCards.cpp
@@ -12,6 +12,25 @@ ControlReplaceCards* ControlReplaceCards::create()
 ControlReplaceCards::ControlReplaceCards()
 {
 	this->replacedCards = new std::set<Card*>();
+	Label* doneButtonLabel = Label::create("Done", Localization::getMainFont(), Localization::getFontSizeP(Localization::getMainFont()));
+	Label* doneButtonLabelHover = Label::create("Done", Localization::getMainFont(), Localization::getFontSizeP(Localization::getMainFont()));
+	Label* doneButtonLabelClick = Label::create("Done", Localization::getMainFont(), Localization::getFontSizeP(Localization::getMainFont()));
+	
+	doneButtonLabel->enableOutline(Color4B::BLACK, 2);
+	doneButtonLabelHover->enableOutline(Color4B::BLACK, 2);
+	doneButtonLabelClick->enableOutline(Color4B::BLACK, 2);
+
+	this->doneButton = TextMenuSprite::create(
+		doneButtonLabel,
+		doneButtonLabelHover,
+		doneButtonLabelClick,
+		Resources::Menus_Buttons_GenericButton,
+		Resources::Menus_Buttons_GenericButtonHover,
+		Resources::Menus_Buttons_GenericButtonClick
+	);
+	this->doneButton->setOpacity(0);
+	
+	this->addChild(this->doneButton);
 }
 
 ControlReplaceCards::~ControlReplaceCards()
@@ -19,11 +38,24 @@ ControlReplaceCards::~ControlReplaceCards()
 	delete(this->replacedCards);
 }
 
+void ControlReplaceCards::initializeListeners()
+{
+	ComponentBase::initializeListeners();
+	this->doneButton->setClickCallback(CC_CALLBACK_1(ControlReplaceCards::onEndReplaceCards, this));
+}
+
+void ControlReplaceCards::onEndReplaceCards(MenuSprite* menuSprite)
+{
+	this->activeGameState->cardReplaceCount = 0;
+	GameState::updateState(this->activeGameState, GameState::StateType::CoinFlip);
+}
+
+
 void ControlReplaceCards::initializePositions()
 {
 	ComponentBase::initializePositions();
-
 	Size visibleSize = Director::getInstance()->getVisibleSize();
+	this->doneButton->setPosition(visibleSize.width / 2.0f + Config::centerColumnCenter, visibleSize.height / 2.0f - 200.0f);
 }
 
 void ControlReplaceCards::onStateChange(GameState* gameState)
@@ -42,6 +74,8 @@ void ControlReplaceCards::onStateChange(GameState* gameState)
 			}
 
 			this->initializeCallbacks(gameState);
+			this->doneButton->runAction(FadeTo::create(Config::replaceEndButtonFadeSpeed, 255));
+			this->doneButton->enableInteraction();
 			break;
 		default:
 			if (gameState->previousStateType == GameState::StateType::ControlReplaceCards)
@@ -61,6 +95,11 @@ void ControlReplaceCards::onStateChange(GameState* gameState)
 					card->setScale(Card::cardScale);
 					gameState->playerDeck->insertCardRandom(card, false, 0.0f);
 				}
+
+				// Hide Done Button
+
+				this->doneButton->runAction(FadeTo::create(Config::replaceEndButtonFadeSpeed, 0));
+				this->doneButton->disableInteraction();
 				break;
 			}
 			break;
@@ -69,7 +108,12 @@ void ControlReplaceCards::onStateChange(GameState* gameState)
 
 void ControlReplaceCards::initializeCardReplace(GameState* gameState)
 {
-	gameState->cardReplaceCount = std::min(3, gameState->playerDeck->getCardCount());
+	if (gameState->round == 0) {
+		gameState->cardReplaceCount = std::min(3, gameState->playerDeck->getCardCount());
+	} else {
+		gameState->cardReplaceCount = std::min(1, gameState->playerDeck->getCardCount());
+	}
+	
 	this->replacedCards->clear();
 
 	Size visibleSize = Director::getInstance()->getVisibleSize();
