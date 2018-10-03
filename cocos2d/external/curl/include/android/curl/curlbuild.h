ba71ba9a0600cc8ed17cfdbc438ad92efd77e3f2
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2018-10-02 18:59:05 -0700

    [cmake-cleanup] remove in-tree cocos2d

--- cocos2d/external/curl/include/android/curl/curlbuild.h
+++
@@ -1,9 +0,0 @@
-#ifdef __aarch64__
-#include "curlbuild-arm64.h"
-#elif __arm__
-#include "curlbuild-arm32.h"
-#elif __i386__
-#include "curlbuild-x86.h"
-#else
-#error "Unsupported architecture!"
-#endif
