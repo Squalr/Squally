839ffe940d6179d1b360d5e5ae374f7e4339ff11
Matthew Bucci <buccim2@gene.com>
2018-10-17 22:23:40 -0700

    switch variable name to defaultValue to make clang happy

--- Source/Engine/Save/SaveManager.h
+++ Source/Engine/Save/SaveManager.h
@@ -14,9 +14,9 @@ public:
 	static void setActiveSaveProfile(ActiveSaveProfile activeSaveProfile);
 	static void saveGlobalData(std::string key, cocos2d::Value data);
 	static void saveProfileData(std::string key, cocos2d::Value data);
-	static cocos2d::Value getGlobalDataOrDefault(std::string key, cocos2d::Value default);
+	static cocos2d::Value getGlobalDataOrDefault(std::string key, cocos2d::Value defaultValue);
 	static cocos2d::Value getGlobalData(std::string key);
-	static cocos2d::Value getProfileDataOrDefault(std::string key, cocos2d::Value default);
+	static cocos2d::Value getProfileDataOrDefault(std::string key, cocos2d::Value defaultValue);
 	static cocos2d::Value getProfileData(std::string key);
 	static bool hasGlobalData(std::string key);
 	static bool hasProfileData(std::string key);
