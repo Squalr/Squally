4f7488988a25f67231a3b54b1d917fa4f7c16956
Zachary Canann <zacharycanann@gmail.com>
2018-11-04 21:35:45 -0800

    Changing victory sound

--- Source/Resources.h
+++ Source/Resources.h
@@ -6172,6 +6172,7 @@ public:
 	static const std::string Sounds_Hexus_Card_Game_Movement_Deal_Single_Small_01;
 	static const std::string Sounds_Hexus_Card_Game_Movement_Tap_01;
 	static const std::string Sounds_Hexus_Card_Game_Movement_Tap_05;
+	static const std::string Sounds_Hexus_ClaimVictory;
 	static const std::string Sounds_Hexus_LastStand;
 	static const std::string Sounds_Hexus_Medieval_War_Horn;
 	static const std::string Sounds_Hexus_UI_CCG_card_downgrade;
