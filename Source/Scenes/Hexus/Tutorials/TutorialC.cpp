73dc43b018915b66990161763add2d982f2a5c82
Zachary Canann <zacharycanann@gmail.com>
2018-11-15 03:21:34 -0800

    Gameboard update, analytics assert removal, tutorial card updates

--- Source/Scenes/Hexus/Tutorials/TutorialC.cpp
+++ Source/Scenes/Hexus/Tutorials/TutorialC.cpp
@@ -15,16 +15,56 @@ TutorialC::TutorialC() : StateOverride(
 	true,
 	false,
 	true,
-	std::vector<CardData*> {},
-	std::vector<CardData*> {},
-	std::vector<CardData*> {},
-	std::vector<CardData*> {},
-	std::vector<CardData*> {},
-	std::vector<CardData*> {},
-	std::vector<CardData*> {},
-	std::vector<CardData*> {},
-	std::vector<CardData*> {},
-	std::vector<CardData*> {})
+	std::vector<CardData*>
+	{
+		CardList::getInstance()->cardListByName->at(CardKeys::Binary0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Binary0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Decimal0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Decimal0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Decimal0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Hex0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Hex0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Hex0),
+	},
+	std::vector<CardData*>
+	{
+		CardList::getInstance()->cardListByName->at(CardKeys::Binary0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Binary0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Decimal0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Decimal0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Hex0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Hex0),
+	},
+	std::vector<CardData*>
+	{
+		CardList::getInstance()->cardListByName->at(CardKeys::Binary12),
+		CardList::getInstance()->cardListByName->at(CardKeys::Decimal11),
+		CardList::getInstance()->cardListByName->at(CardKeys::Hex13),
+	},
+	std::vector<CardData*> { },
+	std::vector<CardData*> { },
+	std::vector<CardData*>
+	{
+		CardList::getInstance()->cardListByName->at(CardKeys::Decimal7),
+	},
+	std::vector<CardData*> { },
+	std::vector<CardData*>
+	{
+		CardList::getInstance()->cardListByName->at(CardKeys::Binary0),
+		CardList::getInstance()->cardListByName->at(CardKeys::Binary8),
+	},
+	std::vector<CardData*>
+	{
+		CardList::getInstance()->cardListByName->at(CardKeys::Decimal11),
+	},
+	std::vector<CardData*>
+	{
+		CardList::getInstance()->cardListByName->at(CardKeys::Hex13),
+		CardList::getInstance()->cardListByName->at(CardKeys::Hex3),
+		CardList::getInstance()->cardListByName->at(CardKeys::Hex1),
+		CardList::getInstance()->cardListByName->at(CardKeys::Hex1),
+		CardList::getInstance()->cardListByName->at(CardKeys::Hex1),
+	})
 {
 }
 
