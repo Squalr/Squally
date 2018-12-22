b1c588875a233827499f9e8edb5f59c4269d9456
Zachary Canann <zacharycanann@gmail.com>
2018-12-21 20:24:55 -0800

    Background is now static regardless of camera zoom

--- Source/Scenes/Maps/Platformer/Backgrounds/Background.h
+++ Source/Scenes/Maps/Platformer/Backgrounds/Background.h
@@ -1,19 +1,17 @@
 #pragma once
-#include "cocos2d.h"
-
 #include "Engine/Maps/SerializableLayer.h"
 
-using namespace cocos2d;
+class Hud;
 
 class Background : public SerializableLayer
 {
 public:
-	static Background* create(ValueMap* initProperties, std::string name, Node* child);
+	static Background* create(cocos2d::ValueMap* initProperties, std::string name, cocos2d::Node* child);
 
 private:
-	Background(ValueMap* properties, std::string name, Node* child);
+	Background(cocos2d::ValueMap* properties, std::string name, Node* child);
 	~Background();
 
-	void visit(Renderer *renderer, const Mat4& parentTransform, uint32_t parentFlags) override;
+	Hud* background;
 };
 
