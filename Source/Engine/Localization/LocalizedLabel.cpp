bc99b7fc716686b3076a0abb4b65f11afa78a65b
Zachary Canann <zacharycanann@gmail.com>
2018-12-22 17:28:48 -0800

    Begin string data refactor

--- Source/Engine/Localization/LocalizedLabel.cpp
+++ Source/Engine/Localization/LocalizedLabel.cpp
@@ -1,1 +1,28 @@
 #include "LocalizedLabel.h"
+
+#include "Engine/Localization/LocalizedString.h"
+
+LocalizedLabel* LocalizedLabel::create()
+{
+	LocalizedLabel* label = new LocalizedLabel();
+
+	label->autorelease();
+
+	return label;
+}
+
+LocalizedLabel::LocalizedLabel()
+{
+	this->localizedString = LocalizedString::create(CC_CALLBACK_1(LocalizedLabel::onLocaleChange, this));
+
+	this->addChild(this->localizedString); // Just adding this to retain it -- this has no visuals
+}
+
+LocalizedLabel::~LocalizedLabel()
+{
+}
+
+void LocalizedLabel::onLocaleChange(std::string newString)
+{
+	this->setString(newString);
+}
--- Source/Engine/Localization/LocalizedLabel.h
+++ Source/Engine/Localization/LocalizedLabel.h
@@ -1,8 +1,29 @@
 #pragma once
+#include <string>
 
-class LocalizedLabel
+#include "cocos/2d/CCLabel.h"
+
+class LocalizedString;
+
+class LocalizedLabel : public cocos2d::Label
 {
 public:
+	enum class FontSize
+	{
+		H1,
+		H2,
+		H3,
+		P,
+		Small,
+	};
+
+	static LocalizedLabel* create();
 
 private:
+	LocalizedLabel();
+	~LocalizedLabel();
+
+	void onLocaleChange(std::string newString);
+
+	LocalizedString* localizedString;
 };
