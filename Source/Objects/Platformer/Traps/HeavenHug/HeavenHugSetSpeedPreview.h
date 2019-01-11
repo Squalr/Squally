020f17f9f635850c3dc54a0bb330c8b7bea8812e
Zachary Canann <zacharycanann@gmail.com>
2019-01-10 18:14:06 -0800

    Resource shuffling, beginning to add heaven hug

---
+++ Source/Objects/Platformer/Traps/HeavenHug/HeavenHugSetSpeedPreview.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Engine/Hackables/HackablePreview.h"
+
+namespace cocos2d
+{
+	class Sprite;
+}
+
+class LocalizedLabel;
+
+class HeavenHugSetSpeedPreview : public HackablePreview
+{
+public:
+	static HeavenHugSetSpeedPreview* create();
+
+	HackablePreview* clone() override;
+
+private:
+	typedef HackablePreview super;
+
+	HeavenHugSetSpeedPreview();
+	~HeavenHugSetSpeedPreview() = default;
+	void onEnter() override;
+	void initializePositions() override;
+
+	cocos2d::Sprite* previewBlade;
+	LocalizedLabel* eax0Degrees;
+	LocalizedLabel* eax90Degrees;
+	LocalizedLabel* eax180Degrees;
+	LocalizedLabel* eax270Degrees;
+};
