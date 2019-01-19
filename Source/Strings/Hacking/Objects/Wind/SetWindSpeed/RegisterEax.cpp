bf7170d639d66c7bfd9d84f4abf38d60fa7ac776
Zachary Canann <zacharycanann@gmail.com>
2019-01-18 21:38:27 -0800

    More wind updates, adding missing x87 FPU register syntax highlighting

--- Source/Strings/Hacking/Objects/Wind/SetWindSpeed/RegisterEax.cpp
+++ Source/Strings/Hacking/Objects/Wind/SetWindSpeed/RegisterEax.cpp
@@ -35,146 +35,146 @@ namespace Strings
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringAr()
 	{
-		return "الزاوية التي سيتأرجح فيها المدرج";
+		return "مؤشر لسرعة الرياح الأفقية ، بين -1.0 و 1.0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringBg()
 	{
-		return "Ъгълът, до който ще се люлее";
+		return "Указател на хоризонталната скорост на вятъра, между -1.0 и 1.0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringCs()
 	{
-		return "Úhel, do kterého se klapka posouvá";
+		return "Ukazatel horizontální rychlosti větru, mezi -1,0 a 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringDa()
 	{
-		return "Den vinkel, som flailen vil svinge";
+		return "En peger på vindens vandrette hastighed, mellem -1,0 og 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringDe()
 	{
-		return "Der Winkel, um den der Schlegel schwingt";
+		return "Ein Zeiger auf die horizontale Windgeschwindigkeit zwischen -1.0 und 1.0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringEl()
 	{
-		return "Η γωνία με την οποία θα στρέφεται η λεπίδα";
+		return "Ένας δείκτης στην οριζόντια ταχύτητα του ανέμου, μεταξύ -1,0 και 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringEn()
 	{
-		return "The angle to which the flail will swing";
+		return "A pointer to the wind's horizontal speed, between -1.0 and 1.0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringEs()
 	{
-		return "El ángulo al que oscilará el mayal.";
+		return "Un puntero a la velocidad horizontal del viento, entre -1.0 y 1.0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringEs419()
 	{
-		return "El ángulo al que oscilará el mayal.";
+		return "Un puntero a la velocidad horizontal del viento, entre -1.0 y 1.0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringFi()
 	{
-		return "Kulma, johon hohto kääntyy";
+		return "Osoitin tuulen vaakasuoraan nopeuteen välillä -1,0 ja 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringFr()
 	{
-		return "L'angle d'inclinaison du fléau";
+		return "Un pointeur sur la vitesse horizontale du vent, entre -1.0 et 1.0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringHu()
 	{
-		return "A szög, amellyel a láng elfordul";
+		return "Egy mutató a szél vízszintes sebességéhez, -1,0 és 1,0 között.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringIt()
 	{
-		return "L'angolo a cui oscillerà il mazzafrusto";
+		return "Un puntatore alla velocità orizzontale del vento, tra -1.0 e 1.0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringJa()
 	{
-		return "Flailが揺れる角度";
+		return "風の水平方向の速度へのポインタ、-1.0から1.0の間";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringKo()
 	{
-		return "흔들기가 스윙 할 각도";
+		return "-1.0에서 1.0 사이의 바람의 수평 속도에 대한 포인터입니다.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringNl()
 	{
-		return "De hoek waaronder de dorsvlegel zal slingeren";
+		return "Een verwijzing naar de horizontale snelheid van de wind, tussen -1,0 en 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringNo()
 	{
-		return "Vinkelen som flailen vil svinge på";
+		return "En peker på vindens horisontale fart, mellom -1,0 og 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringPl()
 	{
-		return "Kąt nachylenia cepa";
+		return "Wskaźnik do poziomej prędkości wiatru w zakresie od -1,0 do 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringPt()
 	{
-		return "O ângulo para o qual o flail vai balançar";
+		return "Um ponteiro para a velocidade horizontal do vento, entre -1,0 e 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringPtBr()
 	{
-		return "O ângulo para o qual o flail vai balançar";
+		return "Um ponteiro para a velocidade horizontal do vento, entre -1,0 e 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringRo()
 	{
-		return "Unghiul la care se va învârti flautul";
+		return "Un indicator al vitezei orizontale a vântului, între -1,0 și 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringRu()
 	{
-		return "Угол, на который будет качаться цепочка";
+		return "Указатель на горизонтальную скорость ветра от -1,0 до 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringSv()
 	{
-		return "Vinkeln som flailen kommer att svänga";
+		return "En pekare till vindens horisontella hastighet, mellan -1,0 och 1,0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringTh()
 	{
-		return "มุมที่ไม้ตีพริกจะแกว่ง";
+		return "ตัวชี้ไปยังความเร็วแนวนอนของลมระหว่าง -1.0 และ 1.0";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringTr()
 	{
-		return "Dosyanın döneceği açı";
+		return "Rüzgârın yatay hızına bir işaret, -1.0 ile 1.0 arasında.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringUk()
 	{
-		return "Кут, до якого буде обертатися хвіст";
+		return "Покажчик на горизонтальну швидкість вітру, від -1.0 до 1.0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringVi()
 	{
-		return "Góc mà con sáo sẽ xoay";
+		return "Một con trỏ tới tốc độ ngang của gió, giữa -1.0 và 1.0.";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringZhCn()
 	{
-		return "连枷摆动的角度";
+		return "指向风的水平速度的指针，介于-1.0和1.0之间。";
 	}
 
 	std::string Hacking_Objects_Wind_SetWindSpeed_RegisterEax::getStringZhTw()
 	{
-		return "連枷擺動的角度";
+		return "指向風的水平速度的指針，介於-1.0和1.0之間。";
 	}
 }
