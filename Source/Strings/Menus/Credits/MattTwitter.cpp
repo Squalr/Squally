c9ac5fae3581f79eb627ea2dc03f38a9d2345688
Zachary Canann <zacharycanann@gmail.com>
2019-03-08 22:49:37 -0800

    Adding credits assets

---
+++ Source/Strings/Menus/Credits/MattTwitter.cpp
@@ -0,0 +1,184 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+// THIS C++ FILE IS GENERATED DO NOT EDIT. RUN GenerateDataFiles.py TO GENERATE THIS FILE //
+////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "MattTwitter.h"
+
+namespace Strings
+{
+	Menus_Credits_MattTwitter* Menus_Credits_MattTwitter::create()
+	{
+		Menus_Credits_MattTwitter* instance = new Menus_Credits_MattTwitter();
+
+		instance->autorelease();
+
+		return instance;
+	}
+	
+	LocalizedString* Menus_Credits_MattTwitter::clone()
+	{
+		LocalizedString* clonedString = Menus_Credits_MattTwitter::create();
+		
+		this->copyAttributesTo(clonedString);
+		
+		return clonedString;
+	}
+
+	Menus_Credits_MattTwitter::Menus_Credits_MattTwitter() : LocalizedString()
+	{
+	}
+
+	Menus_Credits_MattTwitter::~Menus_Credits_MattTwitter()
+	{
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringIdentifier()
+	{
+		return "Menus_Credits_MattTwitter";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringAr()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringBg()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringCs()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringDa()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringDe()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringEl()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringEn()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringEs()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringEs419()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringFi()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringFr()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringHu()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringIt()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringJa()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringKo()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringNl()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringNo()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringPl()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringPt()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringPtBr()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringRo()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringRu()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringSv()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringTh()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringTr()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringUk()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringVi()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringZhCn()
+	{
+		return "@letsRFTM";
+	}
+
+	std::string Menus_Credits_MattTwitter::getStringZhTw()
+	{
+		return "@letsRFTM";
+	}
+}
---
+++ Source/Strings/Menus/Credits/NateTwitter.cpp
@@ -0,0 +1,184 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+// THIS C++ FILE IS GENERATED DO NOT EDIT. RUN GenerateDataFiles.py TO GENERATE THIS FILE //
+////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "NateTwitter.h"
+
+namespace Strings
+{
+	Menus_Credits_NateTwitter* Menus_Credits_NateTwitter::create()
+	{
+		Menus_Credits_NateTwitter* instance = new Menus_Credits_NateTwitter();
+
+		instance->autorelease();
+
+		return instance;
+	}
+	
+	LocalizedString* Menus_Credits_NateTwitter::clone()
+	{
+		LocalizedString* clonedString = Menus_Credits_NateTwitter::create();
+		
+		this->copyAttributesTo(clonedString);
+		
+		return clonedString;
+	}
+
+	Menus_Credits_NateTwitter::Menus_Credits_NateTwitter() : LocalizedString()
+	{
+	}
+
+	Menus_Credits_NateTwitter::~Menus_Credits_NateTwitter()
+	{
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringIdentifier()
+	{
+		return "Menus_Credits_NateTwitter";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringAr()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringBg()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringCs()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringDa()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringDe()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringEl()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringEn()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringEs()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringEs419()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringFi()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringFr()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringHu()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringIt()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringJa()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringKo()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringNl()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringNo()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringPl()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringPt()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringPtBr()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringRo()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringRu()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringSv()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringTh()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringTr()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringUk()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringVi()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringZhCn()
+	{
+		return "@Teknoman117";
+	}
+
+	std::string Menus_Credits_NateTwitter::getStringZhTw()
+	{
+		return "@Teknoman117";
+	}
+}
