9da05bc3553098d6d7ba440c5b44eeed5d382d3f
Zachary Canann <zacharycanann@gmail.com>
2019-03-08 22:49:37 -0800

    Adding credits assets

---
+++ Source/Strings/Menus/Credits/ZacTwitter.cpp
@@ -0,0 +1,184 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+// THIS C++ FILE IS GENERATED DO NOT EDIT. RUN GenerateDataFiles.py TO GENERATE THIS FILE //
+////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "ZacTwitter.h"
+
+namespace Strings
+{
+	Menus_Credits_ZacTwitter* Menus_Credits_ZacTwitter::create()
+	{
+		Menus_Credits_ZacTwitter* instance = new Menus_Credits_ZacTwitter();
+
+		instance->autorelease();
+
+		return instance;
+	}
+	
+	LocalizedString* Menus_Credits_ZacTwitter::clone()
+	{
+		LocalizedString* clonedString = Menus_Credits_ZacTwitter::create();
+		
+		this->copyAttributesTo(clonedString);
+		
+		return clonedString;
+	}
+
+	Menus_Credits_ZacTwitter::Menus_Credits_ZacTwitter() : LocalizedString()
+	{
+	}
+
+	Menus_Credits_ZacTwitter::~Menus_Credits_ZacTwitter()
+	{
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringIdentifier()
+	{
+		return "Menus_Credits_ZacTwitter";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringAr()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringBg()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringCs()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringDa()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringDe()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringEl()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringEn()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringEs()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringEs419()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringFi()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringFr()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringHu()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringIt()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringJa()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringKo()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringNl()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringNo()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringPl()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringPt()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringPtBr()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringRo()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringRu()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringSv()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringTh()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringTr()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringUk()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringVi()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringZhCn()
+	{
+		return "@zcananm";
+	}
+
+	std::string Menus_Credits_ZacTwitter::getStringZhTw()
+	{
+		return "@zcananm";
+	}
+}
