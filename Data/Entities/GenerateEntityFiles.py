20e0ab2d078ecc4b269bd1ec732373f48d940dfd
Zachary Canann <zacharycanann@gmail.com>
2019-03-10 23:14:09 -0700

    Update to entity file generation scripts to support sharded file structure

--- Data/Entities/GenerateEntityFiles.py
+++ Data/Entities/GenerateEntityFiles.py
@@ -1,18 +1,75 @@
 from os import listdir
 from os import path
-from os.path import isfile, join, splitext, abspath, realpath, basename, relpath
+from os.path import isfile, dirname, join, splitext, abspath, realpath, basename, relpath
 import json
 import os
 import re
 
 def main():
-	entityData = parseEntityFile(abspath(join(join(realpath(__file__), ".."), "Entities.json")))
-	generateDeserializationCode(entityData)
+	generateEntityFiles()
 
-def generateDeserializationCode(entityData):
+def generateEntityFiles():
+	searchPath = dirname(realpath(__file__))
+	files = []
+
+	# Read all json files under this directory
+	for root, dirnames, filenames in os.walk(searchPath):
+		for filename in filenames:
+			if filename.lower().endswith(".json"):
+				files.append(join(root, filename))
+				continue
+
+	files.sort()
+
+	allEntityData = []
+
+	# Parse each one and generate the file data
+	for entityDataFile in files:
+		entityData = parseEntityFile(entityDataFile)
+		
+		if entityData:
+			allEntityData.append(entityData)
+
+	generateEntityDeserializationCode(allEntityData)
+
+def parseEntityFile(entityDataPath):
+	# Load the entity JSON file
+	with open(entityDataPath, "r") as dataFile:
+		entityData = json.load(dataFile)
+
+		# Construct meta-variables
+		entityData["Environment"] = relpath(entityDataPath).split("/")[0]
+		entityData["Prefix"] = relpath(entityDataPath).split("/")[1]
+		entityData["Name"] = relpath(entityDataPath).split("/")[2][:-len(".json")]
+
+		if entityData["Prefix"] == "Enemies":
+			entityData["Collision"] = "Enemy"
+			entityData["Base"] = "PlatformerEnemy"
+			entityData["Path"] = "Entities/Platformer/PlatformerEnemy.h"
+		elif entityData["Prefix"] == "Helpers":
+			entityData["Collision"] = "FriendlyNpc"
+			entityData["Base"] = "PlatformerEntity"
+			entityData["Path"] = "Entities/Platformer/PlatformerEntity.h"
+		elif entityData["Prefix"] == "Npcs":
+			entityData["Collision"] = "FriendlyNpc"
+			entityData["Base"] = "NpcBase"
+			entityData["Path"] = "Entities/Platformer/NpcBase.h"
+		elif entityData["Prefix"] == "Misc":
+			entityData["Collision"] = "FriendlyNpc"
+			entityData["Base"] = "PlatformerEntity"
+			entityData["Path"] = "Entities/Platformer/PlatformerEntity.h"
+		else:
+			print("unknown entity type: " + entityData["Type"])
+			return {}
+		
+		return entityData
+
+	return []
+
+def generateEntityDeserializationCode(allEntityData):
 	deserializerClass = abspath(join(join(realpath(__file__), ".."), "../../Source/Entities/Platformer/PlatformerEntityDeserializer.cpp"))
 	
-	with open(deserializerClass,'r+') as contentReader:
+	with open(deserializerClass,"r+") as contentReader:
 		includesPrefixDelimiter = "////V////V////V////V////V////V////V/"
 		includesSuffixDelimiter = "////Y////Y////Y////Y////Y////Y////Y/"
 		prefixDelimiter = "////X////X////X////X////X////X////X/"
@@ -21,74 +78,24 @@ def generateDeserializationCode(entityData):
 		
 		generatedIncludes = "\n\n"
 		
-		for nextEntity in entityData:
-			generatedIncludes += nextEntity["Include"] + "\n"
+		for nextEntity in allEntityData:
+			generatedIncludes += ("#include \"Entities/Platformer/" + nextEntity["Prefix"] + "/" + nextEntity["Environment"]).rstrip("/") + "/" + nextEntity["Name"] + ".h\"" + "\n"
 		
 		contents = replaceTextBetween(includesPrefixDelimiter, includesSuffixDelimiter, contents, generatedIncludes + "\n")
 		generatedContent = "\n\n"
 		
-		for nextEntity in entityData:
-			generatedContent += "\t\t" + "if (name == " + nextEntity["MapKey"] + ")\n"
+		for nextEntity in allEntityData:
+			generatedContent += "\t\t" + "if (name == " + nextEntity["Name"] + "::" + "MapKey" + nextEntity["Name"] + ")\n"
 			generatedContent += "\t\t" + "{\n"
-			generatedContent += "\t\t\t" + "newEntity = " + nextEntity["EntityName"] + "::deserialize(properties);\n"
+			generatedContent += "\t\t\t" + "newEntity = " + nextEntity["Name"] + "::deserialize(properties);\n"
 			generatedContent += "\t\t" + "}\n"
 		
 		contents = replaceTextBetween(prefixDelimiter, suffixDelimiter, contents, generatedContent + "\n\t\t")
 		
-		with open(deserializerClass,'w+') as contentWriter:
+		with open(deserializerClass,"w+") as contentWriter:
 			contentWriter.write(contents)
-
-def parseEntityFile(entityDataPath):
-	entityData = []
 	
-	with open(entityDataPath, 'r') as dataFile:
-		entityDictionary = json.load(dataFile)
-		
-	for entityName in entityDictionary:
-		entityData.append(parseEntity(entityName, entityDictionary[entityName]))
-	
-	return entityData
-	
-def parseEntity(entityName, entityData):
-	# Load data
-	entityType = entityData["Type"]
-	entityScale = entityData["Scale"]
-	entitySize = entityData["Size"]
-	entityOffset = entityData["Offset"]
-	entityHealth = str(entityData["Health"])
-	entitySpecial = str(entityData["Special"])
-	entityEnvironment = entityData["Environment"]
-	
-	# Construct variables from parsed data
-	if entityType == "Enemy":
-		entityPrefix = "Enemies"
-		entityCollisionType = "Enemy"
-		entityBase = "PlatformerEnemy"
-		entityBasePath = "Entities/Platformer/PlatformerEnemy.h"
-	elif entityType == "Player":
-		entityPrefix = "Squally"
-		entityCollisionType = "Player"
-		entityBase = "PlatformerEntity"
-		entityBasePath = "Entities/Platformer/PlatformerEntity.h"
-	elif entityType == "Helper":
-		entityPrefix = "Helpers"
-		entityCollisionType = "FriendlyNpc"
-		entityBase = "PlatformerEntity"
-		entityBasePath = "Entities/Platformer/PlatformerEntity.h"
-	elif entityType == "Npc":
-		entityPrefix = "Npcs"
-		entityCollisionType = "FriendlyNpc"
-		entityBase = "NpcBase"
-		entityBasePath = "Entities/Platformer/NpcBase.h"
-	elif entityType == "Misc":
-		entityPrefix = "Misc"
-		entityCollisionType = "FriendlyNpc"
-		entityBase = "PlatformerEntity"
-		entityBasePath = "Entities/Platformer/PlatformerEntity.h"
-	else:
-		print("unknown entity type: " + entityType)
-		return {}
-	
+def generateEntityCode(entityData):
 	mapKeyName = "-".join(filter(None, re.split("([A-Z][^A-Z]*)", entityName))).lower()
 	pathRoot = abspath(join(join(realpath(__file__), "../../.."), ("Source/Entities/Platformer/" + entityPrefix + "/" + entityEnvironment).rstrip("/"))) + "/"
 	animationFile = "Resources/Platformer/Entities/" + entityPrefix + "/" + "entityName"
@@ -113,7 +120,7 @@ def parseEntity(entityName, entityData):
 	
 	# Parse any user code in non-generated sections
 	if os.path.isfile(pathRoot + outputHeader) and os.path.isfile(pathRoot + outputClass):
-		with open(pathRoot + outputHeader,'r') as h, open(pathRoot + outputClass,'r') as cpp:
+		with open(pathRoot + outputHeader,"r") as h, open(pathRoot + outputClass,"r") as cpp:
 			hContent = h.read()
 			cppContent = cpp.read()
 			hUserContentA = getTextBetween(hPrefixA, hSuffixA, hContent)
@@ -124,11 +131,26 @@ def parseEntity(entityName, entityData):
 			cppUserContentC = getTextBetween(cppPrefixC, cppSuffixC, cppContent)
 	
 	os.makedirs(pathRoot, exist_ok=True)
-	with open(pathRoot + outputHeader,'w+') as h, open(pathRoot + outputClass,'w+') as cpp, open(templateOutputHeader,'r') as hTemplate, open(templateOutputClass,'r') as cppTemplate:
+	with open(pathRoot + outputHeader,"w+") as h, open(pathRoot + outputClass,"w+") as cpp, open(templateOutputHeader,"r") as hTemplate, open(templateOutputClass,"r") as cppTemplate:
 		
 		def parseTemplate(template):
 			templateData = template.read()
-			templateData = templateData.replace("{{EntityName}}", entityName).replace("{{EntityBasePath}}", entityBasePath).replace("{{EntityBase}}", entityBase).replace("{{EntityType}}", entityType).replace("{{EntityEnvironment}}", entityEnvironment).replace("{{EntityPrefix}}", entityPrefix).replace("{{EntityCollisionType}}", entityCollisionType).replace("{{MapKeyName}}", mapKeyName).replace("{{EntityScale}}", entityScale).replace("{{EntityWidth}}", entitySize["Width"]).replace("{{EntityHeight}}", entitySize["Height"]).replace("{{EntityOffsetX}}", entityOffset["X"]).replace("{{EntityOffsetY}}", entityOffset["Y"]).replace("{{EntityHealth}}", entityHealth).replace("{{EntitySpecial}}", entitySpecial)
+			templateData = templateData \
+				.replace("{{EntityName}}", entityData["Name"]) \
+				.replace("{{EntityBasePath}}", entityData["Path"]) \
+				.replace("{{EntityBase}}", entityData["Base"]) \
+				.replace("{{EntityType}}", entityData["Type"]) \
+				.replace("{{EntityEnvironment}}", entityData["Environment"]) \
+				.replace("{{EntityPrefix}}", entityData["Prefix"]) \
+				.replace("{{EntityCollisionType}}", entityData["Collision"]) \
+				.replace("{{MapKeyName}}", mapKeyName) \
+				.replace("{{EntityScale}}", entityData["Scale"]) \
+				.replace("{{EntityWidth}}", entitySize["Width"]) \
+				.replace("{{EntityHeight}}", entitySize["Height"]) \
+				.replace("{{EntityOffsetX}}", entityOffset["X"]) \
+				.replace("{{EntityOffsetY}}", entityOffset["Y"]) \
+				.replace("{{EntityHealth}}", str(entityOffset["Health"])) \
+				.replace("{{EntitySpecial}}", str(entityOffset["Special"]))
 			
 			if entityEnvironment == "":
 				templateData = templateData.replace("{{EnvironmentUnderscore}}", "");
@@ -149,12 +171,6 @@ def parseEntity(entityName, entityData):
 			
 		h.write(hContent)
 		cpp.write(cppContent)
-	
-	return {
-		"EntityName": entityName,
-		"MapKey": entityName + "::" + "MapKey" + entityName,
-		"Include": ("#include \"Entities/Platformer/" + entityPrefix + "/" + entityEnvironment).rstrip("/") + "/" + outputHeader + "\""
-	}
 
 def replaceTextBetween(delimeterA, delimeterB, contents, innerContent):
 	contentsPrefix = contents.split(delimeterA)[0]
@@ -165,5 +181,5 @@ def replaceTextBetween(delimeterA, delimeterB, contents, innerContent):
 def getTextBetween(delimeterA, delimeterB, contents):
 	return contents.split(delimeterA)[1].split(delimeterB)[0]
 	
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
