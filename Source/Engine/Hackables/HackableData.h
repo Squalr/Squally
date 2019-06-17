bf16416e5310ec5ee9f13d73122dc08da742bdf2
Zachary Canann <zacharycanann@gmail.com>
2019-06-17 16:55:17 -0700

    Enforcing eq constraints in radial menu

--- Source/Engine/Hackables/HackableData.h
+++ Source/Engine/Hackables/HackableData.h
@@ -10,7 +10,7 @@ class LocalizedString;
 class HackableData : public HackableAttribute
 {
 public:
-	static HackableData* create(void* dataAddress, float duration, LocalizedString* variableName, const std::type_info& dataTypeInfo, std::string iconResource, HackablePreview* hackablePreview);
+	static HackableData* create(void* dataAddress, int requiredEq, float duration, LocalizedString* variableName, const std::type_info& dataTypeInfo, std::string iconResource, HackablePreview* hackablePreview);
 
 	void* getPointer() override;
 
@@ -19,7 +19,7 @@ protected:
 
 private:
 	typedef HackableAttribute super;
-	HackableData(void* dataAddress, float duration, LocalizedString* variableName, const std::type_info& dataTypeInfo, std::string iconResource, HackablePreview* hackablePreview);
+	HackableData(void* dataAddress, int requiredEq,  float duration, LocalizedString* variableName, const std::type_info& dataTypeInfo, std::string iconResource, HackablePreview* hackablePreview);
 	virtual ~HackableData();
 
 	void* dataPointer;
