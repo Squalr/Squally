c8c25a6de0027234a6bf2b23e5607a17bbdd57b8
Zachary Canann <zacharycanann@gmail.com>
2022-10-02 12:50:13 -0700

    Add gecky/guano swap to EF inn

--- Source/Objects/Platformer/Shops/EndianForest/ShopPoolToben.cpp
+++ Source/Objects/Platformer/Shops/EndianForest/ShopPoolToben.cpp
@@ -6,7 +6,6 @@
 #include "Engine/Inventory/ItemChance.h"
 #include "Engine/Localization/ConstantString.h"
 #include "Engine/Utils/GameUtils.h"
-#include "Objects/Platformer/ItemPools/Tiered/Tier1/CardPoolTier1.h"
 #include "Scenes/Platformer/Inventory/Items/PlatformerItems.h"
 
 #include "Resources/UIResources.h"
