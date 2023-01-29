603ac8c9f5d65ead70dfec01adb34d52ef2d0247
Zachary Canann <zahcarycanann@gmail.com>
2023-01-28 17:56:29 -0800

    LC maps

--- Source/Objects/Platformer/Liquids/Acid.cpp
+++ Source/Objects/Platformer/Liquids/Acid.cpp
@@ -16,8 +16,8 @@
 using namespace cocos2d;
 
 const std::string Acid::MapKey = "acid";
-const Color4B Acid::SurfaceColor = Color4B(0, 224, 0, 255);
-const Color4B Acid::BodyColor = Color4B(12, 212, 12, 240);
+const Color4B Acid::SurfaceColor = Color4B(143, 231, 84, 255);
+const Color4B Acid::BodyColor = Color4B(64, 224, 32, 240);
 const float Acid::AcidGravity = 0.0f;
 const float Acid::BubbleDensityPer1024Px = 1.0f;
 
