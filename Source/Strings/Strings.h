7a7edfbb4e62b628ef34e74890b1e5928ba2f6ee
Zachary Canann <zahcarycanann@gmail.com>
2023-05-06 22:04:36 -0700

    L O C A L I Z A T I O N

--- Source/Strings/Strings.h
+++ Source/Strings/Strings.h
@@ -25269,21 +25269,6 @@ namespace Strings
 		virtual ~Menus_Hacking_Objects_WoodenSpikes_UpdateTimer_UpdateTimer() = default;
 	};
 
-	class Menus_Hacking_RadialMenu_RequiresEq : public LocalizedString
-	{
-	public:
-		static LocalizedString* create();
-		LocalizedString* clone() override;
-		std::string getStringIdentifier() override;
-		
-	protected:
-		std::string getStringByLanguage(cocos2d::LanguageType languageType) override;
-		
-	private:
-		Menus_Hacking_RadialMenu_RequiresEq();
-		virtual ~Menus_Hacking_RadialMenu_RequiresEq() = default;
-	};
-
 	class Menus_Hacking_RegisterEax : public LocalizedString
 	{
 	public:
