92fcf197cc000f4e16d65f411a6d392368793971
Zachary Canann <zahcarycanann@gmail.com>
2023-05-24 21:46:38 -0700

    More stats tuning

--- Source/Scenes/Platformer/Inventory/Items/Equipment/Weapons/Swords/Sword.h
+++ Source/Scenes/Platformer/Inventory/Items/Equipment/Weapons/Swords/Sword.h
@@ -3,7 +3,7 @@
 #include "Scenes/Platformer/Inventory/Items/Equipment/Weapons/Weapon.h"
 
 // Swords: Medium damage variance, medium timeline speed,  balanced hp/mp (favoring hp)
-#define SWORD_RANGE(tier) (int)std::ceil((float)tier * 3.0f / 4.0f) + 1
+#define SWORD_RANGE(tier) (int)std::ceil((float)tier * 0.75f) + 2
 #define SWORD_DAMAGE_MIN(tier) BASE_SCALING(tier) - (tier <= 2 ? tier : 0) - (tier >= 3 && tier <= 4 ? 1 : 0)
 #define SWORD_DAMAGE_MAX(tier) SWORD_DAMAGE_MIN(tier) + SWORD_RANGE(tier)
 
