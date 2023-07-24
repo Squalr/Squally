e2ca91270c734c650fa24d813c18cb0071418bf6
Zachary Canann <zacharycanann@gmail.com>
2023-07-23 19:30:12 -0700

    Steam runtime fixes

--- proj.linux/LoadSteamRuntime.sh
+++ proj.linux/LoadSteamRuntime.sh
@@ -1,6 +1,5 @@
 podman run -it \
 -v /home/zachary/Documents/SteamRuntimeMount:/home/zachary/Documents/SteamRuntimeMount \
--v ~/.ssh:/root/.ssh \
-registry.gitlab.steamos.cloud/steamrt/scout/sdk:latest /bin/bash
+registry.gitlab.steamos.cloud/steamrt/sniper/sdk:latest /bin/bash
 
 
